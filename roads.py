33ba3dcc2c25dc0a85609555afb70f1d79b5fb66
neverbeam <martijndortmond@hotmail.com>
2018-06-24 19:48:05 +0200

    updated mateplotlib functions

--- roads.py
+++ roads.py
@@ -4,6 +4,7 @@ import numpy as np
 import mpld3
 from mpld3 import plugins, utils
 import networkx as nx
+from scipy.misc import imread
 
 class LinkedView(plugins.PluginBase):
     """A simple plugin showing how multiple axes can be linked"""
@@ -71,9 +72,88 @@ def test_plots():
 
     return mpld3.fig_to_html(fig)
 
+def world_plot():
+    fig, ax = plt.subplots(2)
+
+    # scatter periods and amplitudes
+    np.random.seed(0)
+    P = [350, 650, 1200, 1200, 2100]
+    A = [350, 800, 250, 600, 950]
+    x = np.linspace(0, 10, 100)
+    data = np.array([[x, Ai * np.sin(x )]
+                     for Ai in A])
+    points = ax[1].scatter(P, A,
+                           s=200, alpha=0.5)
+    img = imread("databases\pics\worldmap.png")
+    ax[1].imshow(img, zorder=0)
+    ax[1].axis('off')
+    ax[1].set_xlabel('Period')
+    ax[1].set_ylabel('Amplitude')
+
+    # create the line object
+    lines = ax[0].plot(x, 0 * x, '-w', lw=3, alpha=0.5)
+    ax[0].set_ylim(-1, 1)
+
+    ax[0].set_title("Hover over points to see lines")
+
+    # transpose line data and add plugin
+    linedata = data.transpose(0, 2, 1).tolist()
+    plugins.connect(fig, LinkedView(points, lines[0], linedata))
+
+    return mpld3.fig_to_html(fig)
+
+
+def continent_plot(name):
+    fig, ax = plt.subplots(2)
+    # scatter periods and amplitudes
+    np.random.seed(0)
+    P = [350, 650, 1200, 1200, 2100]
+    A = [350, 800, 250, 600, 950]
+    # To be made
+    x = np.linspace(0, 10, 100)
+    data = np.array([[x, Ai * np.sin(x )]
+                     for Ai in A])
+    points = ax[1].scatter(P, A,
+                           s=200, alpha=0.5)
+    img = imread("databases\pics\{}.png".format(name))
+    ax[1].imshow(img, zorder=0)
+    ax[1].axis('off')
+    ax[1].set_xlabel('Period')
+    ax[1].set_ylabel('Amplitude')
+
+    # create the line object
+    lines = ax[0].plot(x, 0 * x, '-w', lw=3, alpha=0.5)
+    ax[0].set_ylim(-1, 1)
+
+    ax[0].set_title("Hover over points to see lines")
+
+    # transpose line data and add plugin
+    linedata = data.transpose(0, 2, 1).tolist()
+    plugins.connect(fig, LinkedView(points, lines[0], linedata))
+
+    return mpld3.fig_to_html(fig)
+
+continent_plot('australiapic')
 
 def test_australia():
     G = nx.read_gpickle('./australia')
     fig = plt.figure()
     nx.draw_networkx(G)
     return mpld3.fig_to_html(fig)
+
+
+
+
+
+
+
+# make the map global rather than have it zoom in to
+# the extents of any plotted data
+ax.set_global()
+ax.stock_img()
+ax.coastlines()
+
+plt.show()
+
+
+    
