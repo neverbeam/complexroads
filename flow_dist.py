7b4de11bc95a32bd236c3e02f58f9ed5c1f2f797
Adriaan de Vries <adriaan.de.vries@hotmail.com>
2018-06-28 19:43:48 +0200

    runnable code

--- flow_dist.py
+++ flow_dist.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from numpy import var
 names = [
     "africa",
     "australia",
@@ -7,14 +8,20 @@ names = [
     "newzealand",
     "northamerica",
     "UK",
-    "road"
+    "road",
+    "smallworld",
+    "erdos",
+    "scalefree"
 ]
 
 paths = ['./Graph_GPickles/' + x + '_FinalResult.gpickle' for x in names]
 
 for name, path in zip(names, paths):
     G = nx.read_gpickle(path)
-    flows = dict(nx.get_edge_attributes(G, 'flow')).values()
-    plt.hist(list(flows), bins = 24, range=(0,6))
+    flows = list(dict(nx.get_edge_attributes(G, 'flow')).values())
+    print(name, var(flows))
+    plt.hist(flows, bins = 24, range=(0,6))
     plt.title(name)
+    plt.xlabel('Capacity')
+    plt.ylabel('Number of roads')
     plt.show()
